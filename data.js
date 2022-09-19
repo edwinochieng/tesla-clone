bc663df41a24621834490eb8f40a3f8391d2c2a1
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-09-19 21:54:10 +0300

    product properties

---
+++ data.js
@@ -0,0 +1,68 @@
+
+export const products = [
+    {
+        id: "0",
+        title : "Model 3",
+        description : "Order Online For Touchless Delivery",
+        backgroundImg : "https://tesla-cdn.thron.com/delivery/public/image/tesla/03e533bf-8b1d-463f-9813-9a597aafb280/bvlatuR/std/4096x2560/M3-Homepage-Desktop-LHD",
+        leftButtonText : "Custom Order",
+        rightButtonText : "Existing Memory",
+        twoButtons : "true",
+        first : "true"
+    },{
+        id: "1",
+        title : "Model Y",
+        description : "Order Online For Touchless Delivery",
+        backgroundImg : "https://tesla-cdn.thron.com/delivery/public/image/tesla/8e2df1b9-a4bf-4eb9-beec-2cf5cc77fca0/bvlatuR/std/2880x2400/Desktop-ModelY?quality=70",
+        leftButtonText : "Custom Order",
+        rightButtonText : "Existing Memory",
+        twoButtons : "true",
+    
+    },{
+        id: "2",
+        title : "Model S",
+        description : "Order Online For Touchless Delivery",
+        backgroundImg : "https://tesla-cdn.thron.com/delivery/public/image/tesla/538ac149-d103-4834-9d38-641d8ae447ef/bvlatuR/std/4096x2560/Homepage-Model-S-Desktop-LHD",
+        leftButtonText : "Custom Order",
+        rightButtonText : "Existing Memory",
+        twoButtons : "true",
+    },{
+        id: "3",
+        title : "Model X",
+        description : "Order Online For Touchless Delivery",
+        backgroundImg : "https://tesla-cdn.thron.com/delivery/public/image/tesla/ddc135ed-1638-40fb-8ab1-f8045059ecef/bvlatuR/std/4096x2560/Homepage-Model-X-Desktop-LHD",
+        leftButtonText : "Custom Order",
+        rightButtonText : "Existing Memory",
+        twoButtons : "true",
+    
+    },{
+        id: "4",
+        title : "Solar Panels",
+        description :  "Lower Cost Solar Panels in America",
+        backgroundImg : "https://tesla-cdn.thron.com/delivery/public/image/tesla/16b04537-a4be-4bf9-8637-86862a858da8/bvlatuR/std/2880x1800/_25-HP-SolarPanels-D",
+        leftButtonText : "Custom Order",
+        rightButtonText : "Existing Memory",
+        twoButtons : "true",
+        
+    }, 
+    {
+        id: "5",
+        title : "Solar Roof",
+        description :  "Produce Clean Energy From Your Roof",
+        backgroundImg : "https://tesla-cdn.thron.com/delivery/public/image/tesla/4a41ec0e-7316-46a8-8ce9-e2f195ba644d/bvlatuR/std/2880x1800/Homepage-SolarRoof-Desktop-Global?quality=auto-medium&format=auto",
+        leftButtonText : "Custom Order",
+        rightButtonText : "Existing Memory",
+        twoButtons : "true",
+    
+    },
+    {
+        id: "6",
+        title : "Accesories",
+        backgroundImg : "https://tesla-cdn.thron.com/delivery/public/image/tesla/dd739764-bcaa-4263-9488-8c73bc9fb046/bvlatuR/std/0x0/dd739764-bcaa-4263-9488-8c73bc9fb046",
+        leftButtonText : "Shop Now",
+        rightButtonText : "Existing Memory",
+        twoButtons : "",
+        
+    }
+    
+]
