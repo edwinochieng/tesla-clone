1e32c8524dd80d304a87dd0136850f7b7b1c2376
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-09-19 18:01:55 +0300

    add props and ExpandMoreIcon

--- components/Items.js
+++ components/Items.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import Button from './Button'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-function Items() {
+function Items({title,description,backgroundImg,leftButtonText,rightButtonText,twoButtons,first}) {
   return (
-    <div>
-        
+    <div className='relative h-screen grid justify-center'>
+        <div className='pt-36'>
+            <div className='text-center'>
+                <p className='font-bold text-3xl'>{title}</p>
+                <div className='pt-2'>
+                    <p className='font-normal'>{description}</p>
+                </div>
+            </div>
+            <div className='absolute bottom-12'>
+                <div className='flex'>
+                   <Button imp="primary" text={leftButtonText}/>
+                   {twoButtons && ( 
+                    <Button imp="secondary" text = {rightButtonText}/>
+                   )}
+                </div>
+                {first&&(
+                    <div className='pt-12'>
+                        <ExpandMoreIcon sx = {{fontSize :30}}/>
+                    </div>
+                )}
+            </div>
+        </div>
     </div>
   )
 }
