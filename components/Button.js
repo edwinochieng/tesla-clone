4ef3ef18b1d71b42df3a166d1dcfefe3f1fba9b1
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-09-19 23:26:53 +0300

    buton styling

--- components/Button.js
+++ components/Button.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Button() {
+function Button({imp,text}) {
   return (
-    <div>Button</div>
+    <div className={`h-10 w-64 bg-opacity-90 m-2 py-1 rounded-md text-center font-medium ${imp == "secondary"? 'bg-white text-black' : 'bg-gray-800  text-white '}`}>{text}</div>
   )
 }
 
